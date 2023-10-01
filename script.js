42fbea09f904124a14ea776c9f5b6af796992fec
Bryce DeSantis <potreader@hotmail.com>
2023-09-30 23:56:22 -0400

    Add hover effect and change styling

--- script.js
+++ script.js
@@ -5,5 +5,8 @@ const center = document.createElement('div')
 for (let i = 0; i < 256; i++){
     const square = document.createElement('div')
     square.classList.add('grid')
+    square.addEventListener('mouseover', (elem) => {
+        elem.target.style.backgroundColor = 'black'
+    })
     container.appendChild(square)
 }
